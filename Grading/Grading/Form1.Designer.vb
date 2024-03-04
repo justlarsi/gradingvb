4b2789d1b50ca308575440ce7501d3cea5293755
Sila <rickyrichie001@gmail.com>
2024-03-04 16:27:13 +0300

    Add files via upload

---
+++ Grading/Grading/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class grading
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.leave = New System.Windows.Forms.Button()
+        Me.grade = New System.Windows.Forms.Button()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.calculate = New System.Windows.Forms.Button()
+        Me.txtgrade = New System.Windows.Forms.TextBox()
+        Me.txtaverage = New System.Windows.Forms.TextBox()
+        Me.txtkiswahili = New System.Windows.Forms.TextBox()
+        Me.txtenglish = New System.Windows.Forms.TextBox()
+        Me.txtmathematics = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.leave)
+        Me.GroupBox1.Controls.Add(Me.grade)
+        Me.GroupBox1.Controls.Add(Me.reset)
+        Me.GroupBox1.Controls.Add(Me.calculate)
+        Me.GroupBox1.Controls.Add(Me.txtgrade)
+        Me.GroupBox1.Controls.Add(Me.txtaverage)
+        Me.GroupBox1.Controls.Add(Me.txtkiswahili)
+        Me.GroupBox1.Controls.Add(Me.txtenglish)
+        Me.GroupBox1.Controls.Add(Me.txtmathematics)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(28, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(623, 357)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Grading System"
+        '
+        'leave
+        '
+        Me.leave.Location = New System.Drawing.Point(414, 266)
+        Me.leave.Name = "leave"
+        Me.leave.Size = New System.Drawing.Size(111, 33)
+        Me.leave.TabIndex = 17
+        Me.leave.Text = "Exit"
+        Me.leave.UseVisualStyleBackColor = True
+        '
+        'grade
+        '
+        Me.grade.Location = New System.Drawing.Point(414, 227)
+        Me.grade.Name = "grade"
+        Me.grade.Size = New System.Drawing.Size(111, 33)
+        Me.grade.TabIndex = 16
+        Me.grade.Text = "Grade"
+        Me.grade.UseVisualStyleBackColor = True
+        '
+        'reset
+        '
+        Me.reset.Location = New System.Drawing.Point(414, 188)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(111, 33)
+        Me.reset.TabIndex = 15
+        Me.reset.Text = "Reset"
+        Me.reset.UseVisualStyleBackColor = True
+        '
+        'calculate
+        '
+        Me.calculate.Location = New System.Drawing.Point(414, 149)
+        Me.calculate.Name = "calculate"
+        Me.calculate.Size = New System.Drawing.Size(111, 33)
+        Me.calculate.TabIndex = 14
+        Me.calculate.Text = "Calculate"
+        Me.calculate.UseVisualStyleBackColor = True
+        '
+        'txtgrade
+        '
+        Me.txtgrade.Location = New System.Drawing.Point(180, 233)
+        Me.txtgrade.Name = "txtgrade"
+        Me.txtgrade.Size = New System.Drawing.Size(181, 26)
+        Me.txtgrade.TabIndex = 13
+        '
+        'txtaverage
+        '
+        Me.txtaverage.Location = New System.Drawing.Point(180, 201)
+        Me.txtaverage.Name = "txtaverage"
+        Me.txtaverage.Size = New System.Drawing.Size(181, 26)
+        Me.txtaverage.TabIndex = 12
+        '
+        'txtkiswahili
+        '
+        Me.txtkiswahili.Location = New System.Drawing.Point(180, 169)
+        Me.txtkiswahili.Name = "txtkiswahili"
+        Me.txtkiswahili.Size = New System.Drawing.Size(181, 26)
+        Me.txtkiswahili.TabIndex = 11
+        '
+        'txtenglish
+        '
+        Me.txtenglish.Location = New System.Drawing.Point(180, 137)
+        Me.txtenglish.Name = "txtenglish"
+        Me.txtenglish.Size = New System.Drawing.Size(181, 26)
+        Me.txtenglish.TabIndex = 10
+        '
+        'txtmathematics
+        '
+        Me.txtmathematics.Location = New System.Drawing.Point(180, 105)
+        Me.txtmathematics.Name = "txtmathematics"
+        Me.txtmathematics.Size = New System.Drawing.Size(181, 26)
+        Me.txtmathematics.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(180, 73)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(181, 26)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(180, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(384, 26)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(38, 233)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Grade"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(38, 201)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(68, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Average"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(38, 168)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Kiswahili"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "English"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Mathematics"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Class"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student's Name"
+        '
+        'grading
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(891, 421)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "grading"
+        Me.Text = "Grading"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtgrade As System.Windows.Forms.TextBox
+    Friend WithEvents txtaverage As System.Windows.Forms.TextBox
+    Friend WithEvents txtkiswahili As System.Windows.Forms.TextBox
+    Friend WithEvents txtenglish As System.Windows.Forms.TextBox
+    Friend WithEvents txtmathematics As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents leave As System.Windows.Forms.Button
+    Friend WithEvents grade As System.Windows.Forms.Button
+    Friend WithEvents reset As System.Windows.Forms.Button
+    Friend WithEvents calculate As System.Windows.Forms.Button
+
+End Class
---
+++ Grading/Grading/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class grading
+    Private Sub calculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles calculate.Click
+        Dim Mathematics As Integer
+        Dim English As Integer
+        Dim Kiswahili As Integer
+        Dim Average As Integer
+        Dim Grade As Char
+
+        If Integer.TryParse(txtmathematics.Text, Mathematics) AndAlso Integer.TryParse(txtenglish.Text, English) AndAlso Integer.TryParse(txtkiswahili.Text, Kiswahili) AndAlso ValidMark(Mathematics) AndAlso ValidMark(English) AndAlso ValidMark(Kiswahili) Then
+            Mathematics = txtmathematics.Text
+            English = txtenglish.Text
+            Kiswahili = txtkiswahili.Text
+            Average = ((Mathematics + English + Kiswahili) / 3)
+            txtaverage.Text = Average.ToString
+
+
+            If Average >= 85 AndAlso Average <= 100 Then
+                Grade = "A"
+            ElseIf Average >= 75 AndAlso Average < 85 Then
+                Grade = "B"
+            ElseIf Average >= 65 AndAlso Average < 75 Then
+                Grade = "C"
+            ElseIf Average >= 55 AndAlso Average < 65 Then
+                Grade = "D"
+            ElseIf Average >= 45 AndAlso Average < 55 Then
+                Grade = "E"
+            Else : Grade = "Fail"
+            End If
+
+            txtgrade.Text = Grade.ToString()
+        Else
+            MessageBox.Show("Please enter valid numeric scores in Mathematics, English and Kiswahili.")
+
+        End If
+    End Sub
+    Private Function ValidMark(ByVal score As Integer) As Boolean
+        Return score >= 1 AndAlso score <= 100
+    End Function
+
+    Private Sub reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles reset.Click
+        txtmathematics.Text = String.Empty
+        txtenglish.Text = String.Empty
+        txtkiswahili.Text = String.Empty
+        txtaverage.Text = String.Empty
+        txtgrade.Text = String.Empty
+        TextBox1.Text = String.Empty
+        TextBox2.Text = String.Empty
+
+
+    End Sub
+    Private Sub grade_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles grade.Click
+        txtgrade.Focus()
+    End Sub
+    Private Sub leave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles leave.Click
+        Me.Close()
+    End Sub
+
+End Class
+
+
